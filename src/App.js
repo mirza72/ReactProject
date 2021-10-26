017bd78e2cff580ada5b55ceda749067eaac13bd
mirza samsin baig <mirzasamsinbaig143@gmail.com>
2021-10-26 20:03:23 +0530

    initi

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+// import Resturant from './component/basics/Resturant';
+// import UseState from './component/Hooks/useState';
+// import UseEffect from './component/Hooks/useEffect';
+import UseReducer from './component/Hooks/useReducer';
+const App = () => {
+  return <UseReducer/>;
+};
+
+export default App;
